069f0c7ac0e85c67d9f1933f5b62a926e43a946a
Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
2016-03-31 11:21:06 +0200

    Updated lang.php

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -3,9 +3,7 @@
  * English language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Christopher Smith <chris@jalakai.co.uk>
+ * @author     Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
  */
- 
-// for admin plugins, the menu prompt to be displayed in the admin menu
-// if set here, the plugin doesn't need to override the getMenuText() method
+
 $lang['menu'] = 'Advanced Configuration'; 
